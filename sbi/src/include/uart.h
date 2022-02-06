7767f3c765349168a0a084747a7ffd5d070cfa3f
Isaac Sikkema <isaacsikkema@gmail.com>
2022-02-06 17:19:01 -0500

    uart ring buffer works

--- sbi/src/include/uart.h
+++ sbi/src/include/uart.h
@@ -3,4 +3,7 @@
 void uart_init(void);
 char uart_get(void);
 void uart_put(char val);
+char uart_get_buffered(void);
+void uart_buffer_write(char c);
 void uart_handle_irq(void);
+void print_buffer(void);
--- sbi/src/uart.c
+++ sbi/src/uart.c
@@ -7,6 +7,14 @@
 #define UART_LCR    (3)
 #define UART_LSR    (5)
 
+#define UART_BUFFER_SIZE (8)
+
+
+char uart_buffer[UART_BUFFER_SIZE];
+int uart_buf_idx = 0;
+int uart_buf_len = 0;
+
+
 void uart_init(void) {
     UART_BASE[UART_LCR] = 0b11; // Set word length to 8
 
@@ -33,7 +41,66 @@ void uart_put(char val) {
     UART_BASE[0] = val;                             // Write data when buffer is ready
 }
 
+char uart_get_buffered(void) {
+    char rv;
+
+    // lock;    Testing with one cpu for now
+
+    if (uart_buf_len > 0) {
+        rv = uart_buffer[uart_buf_idx];
+        
+        uart_buf_idx++;
+        uart_buf_idx %= UART_BUFFER_SIZE;
+
+        uart_buf_len--;
+    } else {
+        rv = 0xff;
+    }
+
+    // unlock
+
+    return rv;
+}
+
+void uart_buffer_write(char c) {
+    int new_idx;
+
+    // lock
+
+    if (uart_buf_len < UART_BUFFER_SIZE) {
+        uart_buf_len++;
+    } else {
+        uart_buf_idx++;
+        uart_buf_idx %= UART_BUFFER_SIZE;
+    }
+
+    new_idx = (uart_buf_idx + uart_buf_len - 1) % UART_BUFFER_SIZE;
+    uart_buffer[new_idx] = c;
+
+    // unlock
+}
+
 void uart_handle_irq(void) {
-    char c = uart_get();
-    uart_put(c);
+    char c;
+    
+    while (1) {
+        c = uart_get();
+        if (c == 0xff) {
+            break;
+        }
+
+        uart_buffer_write(c);
+    }
+}
+
+void print_buffer(void) {
+    int i;
+
+    uart_put('[');
+    for (i = 0; i < UART_BUFFER_SIZE; i++) {
+        uart_put(uart_buffer[i]);
+    }
+
+    uart_put(']');
+    printf("\n");
 }
