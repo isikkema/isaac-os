ec25466ae9c3b35468d3cf14633668e45d0acf8f
Isaac Sikkema <isaacsikkema@gmail.com>
2022-02-08 15:28:19 -0500

    Remove test

--- sbi/src/main.c
+++ sbi/src/main.c
@@ -6,9 +6,6 @@
 #include <lock.h>
 
 
-Barrier barrier = {8};
-Mutex mut;
-
 long int SBI_GPREGS[32][8];
 
 
@@ -19,31 +16,15 @@ static void pmp_init() {
 
 
 int main(int hartid) {
-    // while (hartid != 0) {
-    //     // sleep
-    // }
+    while (hartid != 0) {
+        // sleep
+    }
     
     if (hartid == 0) {
         uart_init();
         plic_init();
         pmp_init();
     }
-
-    for (int i = 0; i < 100000000*hartid; i++) {
-        1000.0 / 3.0;
-    }
-
-    mutex_spinlock(&mut);
-    printf("Hart %d waiting...\n", hartid);
-    mutex_unlock(&mut);
-
-    barrier_spinwait(&barrier);
-
-    mutex_spinlock(&mut);
-    printf("Hart %d got past barrier!\n", hartid);
-    mutex_unlock(&mut);
-
-    while (1) {};
 
     CSR_WRITE("mscratch", &SBI_GPREGS[0][hartid]);
 
