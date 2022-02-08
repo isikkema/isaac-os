a523f23daaf67a559bbee4f3c1f931b0a4761c09
Isaac Sikkema <isaacsikkema@gmail.com>
2022-02-08 00:32:19 -0500

    Add semaphore

--- sbi/src/include/lock.h
+++ sbi/src/include/lock.h
@@ -2,7 +2,15 @@ typedef struct Mutex {
     int state;
 } Mutex;
 
+typedef struct Semaphore {
+    int value;
+} Semaphore;
+
 
 int mutex_trylock(Mutex* mutex);
 void mutex_spinlock(Mutex* mutex);
 void mutex_unlock(Mutex* mutex);
+
+int semaphore_trydown(Semaphore* semaphore);
+void semaphore_spindown(Semaphore* semaphore);
+void semaphore_up(Semaphore* semaphore);
--- sbi/src/lock.c
+++ sbi/src/lock.c
@@ -18,3 +18,26 @@ void mutex_spinlock(Mutex* mutex) {
 void mutex_unlock(Mutex* mutex) {
     asm volatile("amoswap.w.rl zero, zero, (%0)" :: "r"(&mutex->state));
 }
+
+
+int semaphore_trydown(Semaphore* semaphore) {
+    int old;
+
+    asm volatile("amoadd.w %0, %1, (%2)" : "=r"(old) : "r"(-1), "r"(&semaphore->value));
+
+    if (old <= 0) {
+        semaphore_up(semaphore);
+    }
+
+    return old > 0;
+}
+
+void semaphore_spindown(Semaphore* semaphore) {
+    while (!semaphore_trydown(semaphore)) {
+        // spin
+    }
+}
+
+void semaphore_up(Semaphore* semaphore) {
+    asm volatile("amoadd.w zero, %0, (%1)" :: "r"(1), "r"(&semaphore->value));
+}
