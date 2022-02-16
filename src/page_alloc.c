bd4f8c9ae2eedfc81feb5a03fe693da2c05ac656
Isaac Sikkema <isaacsikkema@gmail.com>
2022-02-16 09:57:13 -0500

    Align pages

--- src/page_alloc.c
+++ src/page_alloc.c
@@ -8,9 +8,18 @@ enum PageSize {
     PS_1G = 1 << 30
 };
 
-typedef struct {
+typedef struct Page {
     char data[PS_4K];
 } Page;
+
+typedef struct PageAlloc {
+    char* bk_bytes;
+    Page* pages;
+    int num_pages;
+} PageAlloc;
+
+
+PageAlloc page_alloc_data;
 
 
 void page_alloc_init(void) {
@@ -18,8 +27,7 @@ void page_alloc_init(void) {
     unsigned long pages;
     unsigned long bk_bytes;
 
-    heap_size = _HEAP_END - _HEAP_START + 1;
-    heap_size = 20482;
+    heap_size = _HEAP_END - _HEAP_START;
 
     // bk_bytes + 4096*pages + extra = heap_size
     // bk_bytes = ceil(pages/4)
@@ -33,9 +41,18 @@ void page_alloc_init(void) {
         bk_bytes++;
     }
 
-    printf("bk_bytes: %ld\n", bk_bytes);
-    printf("pages: %ld\n", pages);
-    printf("heap_size: %ld\n", heap_size);
-    printf("used: %ld\n", bk_bytes + pages*4096);
-    printf("extra: %ld\n", heap_size - (bk_bytes + pages*4096));
+    page_alloc_data.bk_bytes = (char*) _HEAP_START;
+    page_alloc_data.pages = (Page*) ((_HEAP_START + bk_bytes + PS_4K - 1) & ~0xFFF);
+    page_alloc_data.num_pages = pages;
+
+    printf("bk_bytes:   %ld\n", bk_bytes);
+    printf("pages:      %ld\n", pages);
+    printf("heap_size:  %ld\n", heap_size);
+    printf("used:       %ld\n", bk_bytes + pages*4096);
+    printf("extra:      %ld\n", heap_size - (bk_bytes + pages*4096));
+
+    printf("start:      0x%08x\n", _HEAP_START);
+    printf("end:        0x%08x\n", _HEAP_END);
+    printf("first page: 0x%08x\n", (unsigned long) page_alloc_data.pages);
+    printf("last page:  0x%08x\n", (unsigned long) (page_alloc_data.pages + page_alloc_data.num_pages - 1));
 }
