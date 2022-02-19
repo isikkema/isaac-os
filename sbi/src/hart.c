4f22b40dcdc087b55e68936d3262c6b3e457b436
Isaac Sikkema <isaacsikkema@gmail.com>
2022-02-19 12:30:10 -0500

    initial start logic

--- sbi/src/hart.c
+++ sbi/src/hart.c
@@ -1,11 +1,14 @@
 #include <hart.h>
 
 
+#define IS_VALID_HART(hart) (hart >= 0 && hart < NUM_HARTS)
+
+
 HartData sbi_hart_data[NUM_HARTS];
 
 
 HartStatus get_hart_status(unsigned int hart) {
-    if (hart >= NUM_HARTS) {
+    if (!IS_VALID_HART(hart)) {
         return HS_INVALID;
     }
 
@@ -13,7 +16,22 @@ HartStatus get_hart_status(unsigned int hart) {
 }
 
 int hart_start(unsigned int hart, unsigned long target, int priv_mode) {
+    priv_mode &= 0b1;
+
     printf("Starting hart %d at 0x%08x in mode %d\n", hart, target, priv_mode);
+
+    if (!IS_VALID_HART(hart)) {
+        return 0;
+    }
+
+    // try lock
     
-    return 0;
+    sbi_hart_data[hart].status = HS_STARTING;
+    sbi_hart_data[hart].target_address = target;
+    sbi_hart_data[hart].priv = priv_mode;
+    // set msip
+
+    // unlock
+
+    return 1;
 }
