68d02bbdce17ce47f67b7d780eefffb091e643cb
Isaac Sikkema <isaacsikkema@gmail.com>
2022-03-08 17:29:39 -0500

    setup common cfg more

--- src/include/virtio.h
+++ src/include/virtio.h
@@ -5,11 +5,25 @@
 #include <rs_int.h>
 
 
-#define VIRTIO_PCI_CAP_CFG_TYPE_COMMON      (1)
-#define VIRTIO_PCI_CAP_CFG_TYPE_NOTIFY      (2)
-#define VIRTIO_PCI_CAP_CFG_TYPE_ISR         (3)
-#define VIRTIO_PCI_CAP_CFG_TYPE_DEVICE      (4)
-#define VIRTIO_PCI_CAP_CFG_TYPE_COMMON_ALT  (5)
+#define VIRTIO_PCI_CAP_CFG_TYPE_COMMON       (1)
+#define VIRTIO_PCI_CAP_CFG_TYPE_NOTIFY       (2)
+#define VIRTIO_PCI_CAP_CFG_TYPE_ISR          (3)
+#define VIRTIO_PCI_CAP_CFG_TYPE_DEVICE       (4)
+#define VIRTIO_PCI_CAP_CFG_TYPE_COMMON_ALT   (5)
+
+#define VIRTIO_DEVICE_STATUS_RESET        (0)
+#define VIRTIO_DEVICE_STATUS_ACKNOWLEDGE  (1 << 0)
+#define VIRTIO_DEVICE_STATUS_DRIVER       (1 << 1)
+#define VIRTIO_DEVICE_STATUS_DRIVER_OK    (1 << 2)
+#define VIRTIO_DEVICE_STATUS_FEATURES_OK  (1 << 3)
+
+#define VIRT_QUEUE_DESC_FLAG_NEXT      (1 << 0)
+#define VIRT_QUEUE_DESC_FLAG_WRITE     (1 << 1)
+#define VIRT_QUEUE_DESC_FLAG_INDIRECT  (1 << 2)
+
+#define VIRT_QUEUE_AVAIL_FLAG_NO_INTERRUPT   (1)
+
+#define VIRT_QUEUE_USED_FLAG_NO_NOTIFY (1)
 
 
 typedef struct virtio_pci_cap {
@@ -43,5 +57,42 @@ typedef struct virtio_pci_cfg_common {
    u64 queue_device;
 } VirtioPciCfgCommon;
 
+typedef struct virt_queue_descriptor {
+   u64 addr;
+   u32 len;
+   u16 flags;
+   u16 next;
+} VirtQueueDescriptor;
+
+typedef struct virt_queue_available {
+   u16 flags;
+   u16 idx;
+   u16 ring[];
+   // u16 used_event;
+} VirtQueueAvailable;
+
+typedef struct virt_queue_used_elem {
+   u32 id;
+   u32 len;
+} VirtQueueUsedElement;
+
+typedef struct virt_queue_used {
+   u16 flags;
+   u16 idx;
+   VirtQueueUsedElement ring[];
+   // u16 avail_event; /* Only if VIRTIO_F_EVENT_IDX */
+} VirtQueueUsed;
+
+typedef struct virtio_rng_device {
+   VirtQueueDescriptor* queue_desc;
+   VirtQueueAvailable* queue_driver;
+   VirtQueueUsed* queue_device;
+   volatile VirtioPciCfgCommon* cfg;
+} VirtioRngDevice;
+
+
+extern VirtioRngDevice virtio_rng_device;
+
+
 bool virtio_setup_capability(volatile EcamHeader* ecam, volatile VirtioPciCapability* cap);
-bool virtio_setup_cap_common(volatile EcamHeader* ecam, volatile VirtioPciCapability* cap);
+bool virtio_setup_cap_cfg_common(volatile EcamHeader* ecam, volatile VirtioPciCapability* cap);
--- src/virtio.c
+++ src/virtio.c
@@ -1,11 +1,17 @@
 #include <virtio.h>
 #include <printf.h>
+#include <kmalloc.h>
+#include <mmu.h>
+
+
+// todo: remove
+VirtioRngDevice virtio_rng_device;
 
 
 bool virtio_setup_capability(volatile EcamHeader* ecam, volatile VirtioPciCapability* cap) {
     switch (cap->cfg_type) {
         case VIRTIO_PCI_CAP_CFG_TYPE_COMMON:
-            if (!virtio_setup_cap_common(ecam, cap)) {
+            if (!virtio_setup_cap_cfg_common(ecam, cap)) {
                 printf("virtio_setup_capability: failed to setup common configuration\n");
                 return false;
             }
@@ -27,10 +33,58 @@ bool virtio_setup_capability(volatile EcamHeader* ecam, volatile VirtioPciCapabi
     return true;
 }
 
-bool virtio_setup_cap_common(volatile EcamHeader* ecam, volatile VirtioPciCapability* cap) {
+bool virtio_setup_cap_cfg_common(volatile EcamHeader* ecam, volatile VirtioPciCapability* cap) {
     volatile VirtioPciCfgCommon* cfg;
+    u16 queue_size;
 
-    printf("barid: %d, bar: 0x%016x, offset: 0x%08x\n", cap->bar, pci_read_bar(ecam, cap->bar), cap->offset);
+    cfg = (VirtioPciCfgCommon*) (pci_read_bar(ecam, cap->bar) + cap->offset);
 
-    return false;
+    cfg->device_status = VIRTIO_DEVICE_STATUS_RESET;
+    if (cfg->device_status != VIRTIO_DEVICE_STATUS_RESET) {
+        printf("virtio_setup_cap_cfg_common: device rejected reset\n");
+        return false;
+    }
+
+    cfg->device_status = VIRTIO_DEVICE_STATUS_ACKNOWLEDGE;
+    if (cfg->device_status != VIRTIO_DEVICE_STATUS_ACKNOWLEDGE) {
+        printf("virtio_setup_cap_cfg_common: device rejected acknowledge\n");
+        return false;
+    }
+
+    cfg->device_status |= VIRTIO_DEVICE_STATUS_DRIVER;
+    if (!(cfg->device_status | VIRTIO_DEVICE_STATUS_DRIVER)) {
+        printf("virtio_setup_cap_cfg_common: device rejected driver\n");
+        return false;
+    }
+
+    // Ignoring features for rng
+
+    cfg->device_status |= VIRTIO_DEVICE_STATUS_FEATURES_OK;
+    if (!(cfg->device_status | VIRTIO_DEVICE_STATUS_FEATURES_OK)) {
+        printf("virtio_setup_cap_cfg_common: device rejected feature ok\n");
+        return false;
+    }
+
+    cfg->queue_select = 0;
+    queue_size = cfg->queue_size;
+
+    virtio_rng_device.queue_desc = kzalloc(queue_size * sizeof(VirtQueueDescriptor));
+    cfg->queue_desc = mmu_translate(kernel_mmu_table, (u64) virtio_rng_device.queue_desc);
+
+    virtio_rng_device.queue_driver = kzalloc(sizeof(VirtQueueAvailable) + sizeof(u16) + queue_size * sizeof(u16));
+    cfg->queue_driver = mmu_translate(kernel_mmu_table, (u64) virtio_rng_device.queue_driver);
+
+    virtio_rng_device.queue_device = kzalloc(sizeof(VirtQueueUsed) + sizeof(u16) + queue_size * sizeof(VirtQueueUsedElement));
+    cfg->queue_device = mmu_translate(kernel_mmu_table, (u64) virtio_rng_device.queue_device);
+
+    cfg->queue_enable = 1;
+    cfg->device_status |= VIRTIO_DEVICE_STATUS_DRIVER_OK;
+    if (!(cfg->device_status | VIRTIO_DEVICE_STATUS_DRIVER_OK)) {
+        printf("virtio_setup_cap_cfg_common: device rejected driver ok\n");
+        return false;
+    }
+
+    virtio_rng_device.cfg = cfg;
+
+    return true;
 }
